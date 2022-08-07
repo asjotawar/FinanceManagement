07dc02e3cea0db4c2bfe7f2cc5144f9e6a1e7302
adityasj <asjotawar@outlook.com>
2022-08-07 04:35:52 -0400

    ntirearchi

---
+++ DataAccessLayer/DbContextClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FinanceManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManagement.DataLayer
+{
+    public class DbContextClass : DbContext
+    {
+        public DbSet<Families_DBO> Families { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=/Users/adityasj/Documents/Repos/FinanceManagement/FinanceManagement/DataLayer/FinanceManagement.db");
+    }
+}
---
+++ Models/DatabaseObjects/Families_DBO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace FinanceManagement.Models
+{
+    public class Families_DBO
+    {
+        public int Id { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
